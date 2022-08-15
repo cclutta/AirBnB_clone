b497d972d17860f4b539850f2123bcd0b9f5087c
cclutta <chelsea.lutta@gmail.com>
2022-08-15 14:26:40 +0300

    Update

--- models/base_model.py
+++ models/base_model.py
@@ -7,16 +7,21 @@ from datetime import datetime
 
 class BaseModel:
     """BaseModel Class. """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Sets public instance attributes
         Args:
-            id : string
-            created_at: datetime
-            updated_at: datetime
+            args: args
+            kwargs: kwargs
         """
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+        if kwargs is not None and kwargs != {}:
+            for k, v in kwargs.items():
+                if k in ["created_at", "updated_at"]:
+                    v = datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f")
+                if k != "__class__":
+                    setattr(self, k, v)
 
     def __str__(self):
         """ Prints [<class name>] (<self.id>) <self.__dict__>"""
